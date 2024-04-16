e6ffdfff8efff3e9796302f0d776374b4825da71
William Kunitake <william@bulkdesign.com.br>
2024-04-16 07:25:42 -0700

    Condition to check the API before calling it

--- blocks/top-bar/top-bar.php
+++ blocks/top-bar/top-bar.php
@@ -11,42 +11,44 @@
     <div class="top-bar-inner">
         <?php if( get_field( 'waiting_time' ) ): ?>
         <ul class="top-bar-content">
-            
+            <?php
+                function fetchData() {
+                    $url = "http://www2.hnsg.org.br/app/api/papediatrico";
+                    $host = parse_url($url, PHP_URL_HOST);
+                    $port = parse_url($url, PHP_URL_PORT) ?: 80;
+                    
+                    // Check if the host is reachable
+                    $connection = @fsockopen($host, $port, $errno, $errstr, 5);
+                    if (!$connection) {
+                        // Return error status and message
+                        return array('status' => 'error', 'message' => "Cannot connect to $host: $errstr");
+                    }
+                    fclose($connection);
+                    
+                    // Create HTTP context with header
+                    $context = stream_context_create(array('http' => array('header' => 'Connection: close\r\n')));
+                    
+                    // Attempt to fetch data
+                    $jsondata = @file_get_contents($url, false, $context);
+                    
+                    // Decode JSON data
+                    $array = json_decode($jsondata, true);
+                    
+                    // Return success status and data
+                    return array('status' => 'success', 'data' => $array);
+                }
+            ?>
+
+            <?php $result = fetchData();
+                
+            if ($result['status'] == 'success') : ?>
                 <li>
                     <p>Tempo de Espera:</p>
-                    <p>PA Pediátrico é de <strong>3 horas</strong></p>
+                    <p>PA Pediátrico é de <strong><?php if($array[0]->HR != "00h") : echo $array[0]->HR; endif; echo $array[0]->MI; ?></strong> | PA Adulto é de <?php if($array[0]->HR_ADULTO != "00h") : echo $array[0]->HR_ADULTO; endif; echo $array[0]->MI_ADULTO; ?></p>
                 </li>
-
-                <!-- <li>
-                    <?php 
-                        //$context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
-                        //$jsondata = file_get_contents("http://www2.hnsg.org.br/app/api/papediatrico", false, $context);
-                        //$array = json_decode($jsondata);
-                    ?>
-
-                    <p>
-                        PA Pediátrico é de
-                        <strong>
-                            <?php		 
-                                //if($array[0]->HR != "00h") : 
-                                //    echo $array[0]->HR; 
-                                //endif;
-                                //    echo $array[0]->MI; 
-                            ?>	 	
-                        </strong>
-                        
-                        | PA Adulto é de
-                        
-                        <strong>
-                            <?php 
-                                //if($array[0]->HR_ADULTO != "00h") : 
-                                //    echo $array[0]->HR_ADULTO; 
-                                //endif;
-                                //    echo $array[0]->MI_ADULTO; 
-                            ?> 	
-                        </strong>
-                    </p>
-                </li> -->
+                <?php else :
+                    echo "Não foi possível conectar à API de espera."; ?>
+            <?php endif; ?>
 
         </ul>
         <?php endif; ?>
