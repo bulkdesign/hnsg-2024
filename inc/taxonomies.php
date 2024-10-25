39abc32b242118f96ad5b2a8d773ff6f17d9d429
William Kunitake <william@bulkdesign.com.br>
2024-10-25 14:54:43 -0700

    Removed taxonomies

--- inc/taxonomies.php
+++ inc/taxonomies.php
@@ -2,32 +2,5 @@
 /**
  * Declares theme's custom taxonomies
  *
- * @package mm
+ * @package bulk
  */
-
-/**
- * Theme's taxonomies
- */
-
-add_action( 'init', 'medicos_tax' );
-function medicos_tax() {
-
-	register_taxonomy(
-		'especialidade',
-		'medico',
-		array(
-			'label' => 'Especialidades',
-			'hierarchical' => true,
-			)
-		);
-
-	register_taxonomy(
-		'centro',
-		'medico',
-		array(
-			'label' => 'Centros',
-			'hierarchical' => true,
-			)
-		);
-
-}
