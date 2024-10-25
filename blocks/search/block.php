94c8d66c730cda58916cb3b9da982b1477ccab4d
William Kunitake <william@bulkdesign.com.br>
2024-10-25 14:54:21 -0700

    Condition to add the search to the end

--- blocks/search/block.php
+++ blocks/search/block.php
@@ -25,4 +25,15 @@ function child_theme_block_search() {
 	);
 }
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['s'])) {
+    $search_query = sanitize_text_field($_POST['s']);
+    
+    // Manually build the redirect URL without using esc_url() to prevent over-encoding
+    $redirect_url = home_url( '/hospital/corpo-clinico/?paged=1&filter_block[s]=' . urlencode($search_query) );
+
+    // Perform the redirect
+    wp_redirect($redirect_url);
+    exit;
+}
+
 add_action( 'theme_declare_block', 'child_theme_block_search', 20 );
