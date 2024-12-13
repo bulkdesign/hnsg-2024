91fcfa74f6ecd97bcb24f7aecbdb8088dfbcd468
William Kunitake <william@bulkdesign.com.br>
2024-12-13 07:13:20 -0800

    Fixed the convenios query showing everywhere

--- blocks/posts-archive-with-filter/posts-archive-with-filter.php
+++ blocks/posts-archive-with-filter/posts-archive-with-filter.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Block: Doc Search with Filter
+ * Block: Posts Archive with Filter
  *
- * @package mm
+ * @package bulk
  */
 
 $filters = get_query_var('filter_block');
@@ -17,102 +17,97 @@ if ( ! is_singular() && empty( $filters ) ) {
     }
 }
 
-if ( is_search() || empty( $filters ) || ! empty( $filters['s'] ) ) {
-    $post_types = get_field( 'post_types' ) ? get_field( 'post_types' ) : apply_filters( 'posts_archive_with_filter_post_types', array( 'post', 'page' ) );
-} else {
-    $post_types = get_field( 'post_types' ) ? get_field( 'post_types' ) : apply_filters( 'posts_archive_with_filter_post_types', array( 'post', 'page' ) );
+// Add a fallback in case $queried_object is still not set
+if ( ! isset( $queried_object ) ) {
+    $queried_object = null; // Define a default value if $queried_object is not set
 }
+
+$post_types = get_field( 'post_types' ) ? get_field( 'post_types' ) : apply_filters( 'posts_archive_with_filter_post_types', array( 'post' ) );
 
 ?>
 
 <section <?php theme_block_attributes( $block, 'posts-archive-with-filter alignwide' ); ?>>
+    <?php if ( ! get_field( 'hide_title' ) ) : ?>
+    <header>
+        <h1>
+            <?php 
+                if( is_search() ){
+                    $title = sprintf( esc_attr__( 'Search for: %s', 'bulk' ), get_search_query() );
+                }elseif( is_home() ){
+                    $title = get_the_title( get_option( 'page_for_posts', true ) );
+                }else{
+                    $title = get_the_archive_title(); 
+                }
 
-    <?php if ( ! get_post_type() === 'convenios' ) : ?>
-        <div class="posts-archive-with-filter-inner">
-            <div class="posts-archive-with-filter-content">
-                <form class="posts-archive-with-filter-filters">
-                    <?php if ( ! get_field( 'hide_title' ) ) : ?>
-                        <h1>
-                            <?php if( get_field( 'title' ) ) {
-                                the_field( 'title' );
-                            } ?>
-                        </h1>
-                    <?php endif; ?>
-
-                    <div class="posts-archive-with-filter-filters-search">
-                        <button type="submit" aria-label="<?php echo esc_attr( 'Digite aqui o nome de um(a) médico(a) ou utilize o filtro para buscar pelas especialidades', 'bulk' ); ?>">
-                            <?php theme_block_asset( 'img/search.svg' ); ?>
-                        </button>
-                        <input type="text" value="<?php echo get_search_query(); ?>" name="filter_block[s]" id="s" placeholder="<?php echo esc_attr( 'Digite aqui o nome de um(a) médico(a) ou utilize o filtro para buscar pelas especialidades', 'bulk' ); ?>" />
-                        <div class="posts-archive-with-filter-filters-submit">
-                            <button type="submit" class="primary-button">
-                                <?php esc_attr_e( 'Apply', 'bulk' ); ?>
-                            </button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
+                if( get_field( 'title' ) ) {
+                    the_field( 'title' );
+                }else{
+                    echo apply_filters( 'posts_archive_with_filter_title', $title );
+                }
+            ?>
+        </h1>
+    </header>
     <?php endif; ?>
 
-    <div class="posts-archive-with-filter-content-wrapper">
+    <?php
+        if( is_post_type_archive() ){
+            $post_type_taxonomies = get_object_taxonomies( $queried_object->name );
+        }elseif( is_tax() ){
+            $post_type_taxonomies = array( $queried_object->taxonomy );
+        }else{
+            $post_type_taxonomies = get_object_taxonomies( $post_types );
+        }
 
+        $filter_taxonomies = get_field( 'taxonomies' ) ? get_field( 'taxonomies' ) : apply_filters( 'posts_archive_with_filter_taxonomies', $post_type_taxonomies, $queried_object );
+    ?>
+    
+    <div class="posts-archive-with-filter-content-wrapper">
+		<div class="posts-archive-with-filter-content-loading">
+			<?php theme_block_asset( 'img/loading.svg' ); ?>
+		</div>
         <div class="posts-archive-with-filter-content-inner">
             <?php
-                $args = array(
-                    'post_type'      => $post_types,
-                    'paged'          => ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1,
-                    'orderby'        => 'title',
-                    'order'          => 'asc',
-                    // phpcs:ignore
-                    'posts_per_page' => get_field( 'posts_per_page' ) ? get_field( 'posts_per_page' ) : 12,
-                    // phpcs:ignore
-                    'tax_query'      => array(),
-                    'ignore_sticky_posts' => 0,
-                );
+                if ( ! $uses_custom_query ) {
+                    global $wp_query;
+			        $query = $wp_query;
+                } else {
+                    $args = array(
+                        'post_type'      => $post_types,
+                        'paged'          => ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1,
+                        'orderby'        => 'date',
+                        'order'          => 'desc',
+                        // phpcs:ignore
+                        'posts_per_page' => get_field( 'posts_per_page' ) ? get_field( 'posts_per_page' ) : get_option( 'posts_per_page' ),
+                        // phpcs:ignore
+                        'tax_query'      => array(),
+                    );
 
-                if ( get_search_query() ) {
-                    $args['s'] = get_search_query();
-                    $args['orderby'] = 'relevance';
-                }
-
-                if ( ! empty( $filters['s'] ) ) {
-                    $args['s'] = $filters['s'];
-                }
-
-                if ( ! empty( $filters['taxonomy'] ) ) {
-                    foreach ( $filters['taxonomy'] as $key => $filter ) {
-                        $args['tax_query'][] = array(
-                            'taxonomy' => $key,
-                            'field'    => 'slug',
-                            'terms'    => $filter,
-                        );
+                    if ( ! empty( $filters['s'] ) ) {
+                        $args['s'] = $filters['s'];
                     }
+
+                    if ( ! empty( $filters['taxonomy'] ) ) {
+                        foreach ( $filters['taxonomy'] as $key => $filter ) {
+                            $args['tax_query'][] = array(
+                                'taxonomy' => $key,
+                                'field'    => 'slug',
+                                'terms'    => $filter,
+                            );
+                        }
+                    }
+
+				    $query = new WP_Query( $args );
                 }
-
-                $query = new WP_Query( $args );
             ?>
 
-            <?php
-                $convenios = array(
-                    'post_type'      => 'convenios',
-                    'orderby'        => 'title',
-                    'order'          => 'asc',
-                    'posts_per_page' => -1,
-                );
-
-                $queryConvenios = new WP_Query( $convenios );
-            ?>
-
-            <?php if ( ! get_post_type() === 'convenios' || ! is_archive() ) : ?>
-                <?php if ( $query->have_posts() ) : ?>
+            <?php if ( $query->have_posts() ) : ?>
+                <?php if ( get_post_type() !== 'convenios' ) : ?>
                     <div class="posts-archive-with-filter-content-grid" id="<?php echo esc_attr( theme_block_id( $block ) ); ?>-posts-archive-with-filter-content-grid" tabindex="0">
                         <?php while ( $query->have_posts() ) : ?>
                             <?php $query->the_post(); ?>
 
                             <?php if ( get_the_title() ) : ?>
                                 <article class="post-loop post-card">
-
                                     <div class="posts-archive-with-filter-content-grid-inner">
                                         <!-- THUMBNAIL -->
                                         <?php if ( has_post_thumbnail() ) : ?>
@@ -162,7 +157,6 @@ if ( is_search() || empty( $filters ) || ! empty( $filters['s'] ) ) {
                             <?php endif; ?>
                         <?php endwhile; ?>
                     </div>
-
                     <div class="posts-archive-with-filter-content-pagination">
                         <?php if ( ! $uses_custom_query ) : ?>
                             <?php the_posts_pagination(); ?>
@@ -177,8 +171,6 @@ if ( is_search() || empty( $filters ) || ! empty( $filters['s'] ) ) {
                                                 'format'  => '?paged=%#%',
                                                 'current' => ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1,
                                                 'total'   => $query->max_num_pages,
-                                                'end_size' => 0,
-                                                'mid_size' => 6,
                                             )
                                         )
                                     );
@@ -187,56 +179,57 @@ if ( is_search() || empty( $filters ) || ! empty( $filters['s'] ) ) {
                         </nav>
                         <?php endif; ?>
                     </div>
-                    
-                <?php else : ?>
-                    <div class="posts-archive-with-filter-content-not-found" id="<?php echo esc_attr( theme_block_id( $block ) ); ?>-posts-archive-with-filter-content-grid" tabindex="0">
-                        <h3><?php esc_attr_e( 'Nothing found', 'bulk' ); ?></h3>
-                        <p><?php esc_attr_e( 'No content matching your selection was found. Please try again.', 'bulk' ); ?></p>
-                    </div>
                 <?php endif; ?>
-            <?php endif; ?>
-
-            <?php if ( ! is_search() ) : ?>
-                <?php if ( $queryConvenios->have_posts() ) : ?>
-                    <div class="posts-archive-with-filter-content-grid" id="<?php echo esc_attr( theme_block_id( $block ) ); ?>-posts-archive-with-filter-content-grid" tabindex="0">
-                        <?php while ( $queryConvenios->have_posts() ) : ?>
-                            <?php $queryConvenios->the_post(); ?>
-
-                            <?php if ( get_the_title() ) : ?>
-                                <article class="post-loop post-card">
-
-                                    <div class="posts-archive-with-filter-content-grid-inner">
-                                        <!-- THUMBNAIL -->
-                                        <?php if ( has_post_thumbnail() ) : ?>
-                                            <div class="posts-archive-with-filter-content-grid-inner-thumb">
-                                                <?php $attachment_image = wp_get_attachment_url( get_post_thumbnail_id() ); ?>
-
-                                                <div class="post-loop-image">
-                                                    <img src="<?php echo esc_url( $attachment_image ); ?>" />
-                                                </div>
-                                            </div>
-                                        <?php endif; ?>
-
-                                        <div class="posts-archive-with-filter-content-grid-inner-content">
-                                            <h3><?php the_title(); ?></h3>
-                                            <a href="<?php the_permalink(); ?>" class="read-more">
-                                                <span class="primary-button"><?php esc_html_e( 'Mais informações', 'bulk' ); ?></span>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </article>
-                            <?php endif; ?>
-                        <?php endwhile; ?>
-                    </div>
-                <?php else : ?>
+            <?php else : ?>
                 <div class="posts-archive-with-filter-content-not-found" id="<?php echo esc_attr( theme_block_id( $block ) ); ?>-posts-archive-with-filter-content-grid" tabindex="0">
                     <h3><?php esc_attr_e( 'Nothing found', 'bulk' ); ?></h3>
                     <p><?php esc_attr_e( 'No content matching your selection was found. Please try again.', 'bulk' ); ?></p>
                 </div>
-                <?php endif; ?>
             <?php endif; ?>
 
+            <?php
+                $convenios = array(
+                    'post_type'      => 'convenios',
+                    'orderby'        => 'title',
+                    'order'          => 'asc',
+                    'posts_per_page' => -1,
+                );
+
+                $queryConvenios = new WP_Query( $convenios );
+            ?>
+
+            <?php if ( $queryConvenios->have_posts() && get_post_type() === 'convenios' ) : ?>
+                <div class="posts-archive-with-filter-content-grid" id="<?php echo esc_attr( theme_block_id( $block ) ); ?>-posts-archive-with-filter-content-grid" tabindex="0">
+                    <?php while ( $queryConvenios->have_posts() ) : ?>
+                        <?php $queryConvenios->the_post(); ?>
+
+                        <?php if ( get_the_title() ) : ?>
+                            <article class="post-loop post-card">
+
+                                <div class="posts-archive-with-filter-content-grid-inner">
+                                    <!-- THUMBNAIL -->
+                                    <?php if ( has_post_thumbnail() ) : ?>
+                                        <div class="posts-archive-with-filter-content-grid-inner-thumb">
+                                            <?php $attachment_image = wp_get_attachment_url( get_post_thumbnail_id() ); ?>
+
+                                            <div class="post-loop-image">
+                                                <img src="<?php echo esc_url( $attachment_image ); ?>" />
+                                            </div>
+                                        </div>
+                                    <?php endif; ?>
+
+                                    <div class="posts-archive-with-filter-content-grid-inner-content">
+                                        <h3><?php the_title(); ?></h3>
+                                        <a href="<?php the_permalink(); ?>" class="read-more">
+                                            <span class="primary-button"><?php esc_html_e( 'Mais informações', 'bulk' ); ?></span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </article>
+                        <?php endif; ?>
+                    <?php endwhile; ?>
+                </div>
+            <?php endif; ?>
         </div>
     </div>
-
 </section>
